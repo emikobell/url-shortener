d7f122d53062594cb0d7c009e2f6652a1416c31a
Emiko Bell <emikoelizabethbell@gmail.com>
2023-07-10 20:39:21 -0700

    Fixed Base 62 encoder

--- src/main/java/com/emikobell/urlshortener/Encode.java
+++ src/main/java/com/emikobell/urlshortener/Encode.java
@@ -1,13 +1,15 @@
 package com.emikobell.urlshortener;
 
 public class Encode {
-    static String encodeBase64(int base10) {
-        String alphaNum = "012345689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String hashString = "";
 
+    public static final String ALPHANUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    static String encodeBase62(long base10) {
+        String hashString = "";
         while (base10 > 0) {
-            hashString = alphaNum.charAt(base10 % 62) + hashString;
-            base10 = base10/64;
+            int rem = Math.toIntExact(base10 % 62);
+            hashString = ALPHANUM.charAt(rem) + hashString;
+            base10 = base10 / 62;
         }
         return hashString; 
     }
--- src/main/java/com/emikobell/urlshortener/UrlShortenerController.java
+++ src/main/java/com/emikobell/urlshortener/UrlShortenerController.java
@@ -1,6 +1,6 @@
 package com.emikobell.urlshortener;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UrlShortenerController {
     
     private static final String template = "http://localhost:8080/%s";
-    private final AtomicInteger counter = new AtomicInteger();
+    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/urlshortener")
     public UrlShortener urlshortener(@RequestParam(value = "url", defaultValue = "test") String url) {
@@ -18,10 +18,9 @@ public class UrlShortenerController {
         // First, search db to make sure that the link doesn't already exist
         // If it does, return the shortened link already in db
         // If not, execute the following
-        int id = counter.incrementAndGet();
-        String shortUrl = Encode.encodeBase64(id);
+        long id = counter.incrementAndGet();
+        String shortUrl = Encode.encodeBase62(id);
         // Insert link into db
-        System.out.println(shortUrl);
         return new UrlShortener(id, String.format(template, shortUrl));
     }
     // Have URL decoder mapping as well
