4305da35770208f1a64e328da9b59f15a8e95010
Emiko Bell <emikoelizabethbell@gmail.com>
2023-07-28 17:49:13 -0700

    Update encode unit tests

--- src/test/java/com/emikobell/urlshortener/EncodeTests.java
+++ src/test/java/com/emikobell/urlshortener/EncodeTests.java
@@ -9,16 +9,42 @@ public class EncodeTests {
 
     @Test
     public void encodedStringIsBase62() throws Exception {
+        
+        // given
         long testNum = 9999999999999L;
-        String encodedString = Encode.encodeBase62(testNum);
         String expectedString = "2q3Rktod";
+
+        // when
+        String encodedString = Encode.encodeBase62(testNum);
+
+        // then
         assertEquals(encodedString, expectedString);
     }
 
     @Test
     public void encodeAnyLongToBase62() throws Exception {
+
+        // given 
         long testNum = new Random().nextLong();
+
+        // when
         String encodedString = Encode.encodeBase62(testNum);
+
+        // then
         assertNotNull(encodedString);
+    }
+
+    @Test
+    public void decodedStringIsBase10() throws Exception {
+
+        // given
+        String testBase64 = "5AlM98a";
+        long expectedLong = 293862272376L;
+
+        // when
+        long decodedLong = Encode.decodeBase62(testBase64);
+
+        // then
+        assertEquals(decodedLong, expectedLong);
     }
 }
